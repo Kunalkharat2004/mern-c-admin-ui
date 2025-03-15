8a4feae92b7dcd5bcd23075676bc7a151539e7dd
Kunal <kunalkharat2004@gmail.com>
2025-03-15 21:41:24 +0530

    add AuthLayout and DashboardLayout components; update router configuration for nested routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage/login";
+import DashboardLayout from "./layouts/DashboardLayout";
+import AuthLayout from "./layouts/AuthLayout";
 
 const router = createBrowserRouter([
     {
-        path: "/",
-        element: <HomePage />
+        path:"/",
+        element:<DashboardLayout />,
+        children:[
+            {
+                path: "",
+                element: <HomePage />
+            },
+        ]
     },
+
     {
-        path: "/auth/login",
-        element: <LoginPage />
+        path: "/auth",
+        element: <AuthLayout />,
+        children: [
+            {
+                path: "login",
+                element: <LoginPage/>
+            }
+        ]
     },
 ])
 
